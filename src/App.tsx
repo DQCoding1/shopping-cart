a489f3ceab0944f07eea985095d6454849dc636b
DQCoding1 <diegoabquintero@gmail.com>
2023-06-27 13:30:29 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"                                     
+import "primereact/resources/themes/lara-dark-blue/theme.css"
+import "primereact/resources/primereact.min.css";  
+
+function App() {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
